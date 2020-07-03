c8cdf808602d5c28e5b6f119bb3ff4c0e54dc452
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-03 16:42:23 +0700

    implement AuthenticationEvent

--- lib/bloc/authentication_event.dart
+++ lib/bloc/authentication_event.dart
@@ -1,4 +1,12 @@
 part of 'authentication_bloc.dart';
 
-@immutable
-abstract class AuthenticationEvent {}
+abstract class AuthenticationEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationStarted extends AuthenticationEvent {}
+
+class AuthenticationLoggedIn extends AuthenticationEvent {}
+
+class AuthenticationLoggedOut extends AuthenticationEvent {}
